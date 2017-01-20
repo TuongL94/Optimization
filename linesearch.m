0856edab82e4fad2e69ca710d08dbeb74bd95afd
TuongL94 <tpi13tla@student.lu.se>
2017-01-20 11:08:53 +0100

    Update linesearch.m

--- linesearch.m
+++ linesearch.m
@@ -8,12 +8,23 @@ function [lambda, No_of_iterations] = linesearch(func,x,d)
 %          No_of_iterations - number of iterations made by the line search
 %          algorithm (always 1)
 
-No_of_iterations = 1;
+No_of_iterations = 0;
 F_zero = func(x);
 lambda = 1;
-alpha = 2;
-epsilon = 0.7;
-h = 1/realmax; % Step size used for derivative approximation
+alpha = 2.0;
+epsilon = 0.45;
+
+% Approximation of the step size that will be used in the derivative
+% approximation
+while func(x+lambda*d) >= F_zero || func(x+alpha*lambda*d) <= F_zero
+    if func(x+lambda*d) > F_zero
+        lambda = lambda/alpha;
+    else
+        lambda = lambda*alpha;
+    end
+end
+
+h = 0.01*lambda; % Step size used for derivative approximation
 fprim0 = (func(x+h*d) - func(x))/h; % Approximation of the derivative of func with forward difference
 T = @(t) F_zero + epsilon*t*fprim0;
 
